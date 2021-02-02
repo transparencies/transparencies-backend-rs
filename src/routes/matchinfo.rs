32edd42d106351a133932b5edf5c1ba8d2851cf2
Simon <simon@systemli.org>
2021-02-02 10:35:24 +0100

    Add notes

--- src/routes/matchinfo.rs
+++ src/routes/matchinfo.rs
@@ -12,6 +12,9 @@ pub struct MatchInfoRequest {
 
 #[get("/matchinfo/{id_type}/{id_number}")]
 pub async fn matchinfo(obj: web::Path<MatchInfoRequest>) -> Result<HttpResponse> {
+    // Call into data_processing with MatchInfoRequest as a Parameter
+
+    // Respond with Information from data_processing
     Ok(HttpResponse::Ok().json(MatchInfoRequest {
         id_type: obj.id_type.to_string(),
         id_number: obj.id_number.to_string(),
