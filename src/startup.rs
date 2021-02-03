ba22be12899a6116c57b40ef633acb840e005c52
Simon <simon@systemli.org>
2021-02-03 09:32:46 +0100

    Cleanup

--- src/startup.rs
+++ src/startup.rs
@@ -1,22 +1,6 @@
-use crate::routes::{
-    health_check,
-    matchinfo,
-};
-use actix_web::{
-    dev::Server,
-    middleware,
-    web,
-    web::Data,
-    App,
-    HttpServer,
-};
-use log::{
-    debug,
-    error,
-    info,
-    trace,
-    warn,
-};
+use crate::routes::{health_check, matchinfo};
+use actix_web::{dev::Server, middleware, web, web::Data, App, HttpServer};
+use log::{debug, error, info, trace, warn};
 
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
@@ -24,17 +8,14 @@ use tracing_actix_web::TracingLogger;
 /// Entrypoint for the library part of the Executable's main function
 
 pub fn run(
-    listener: TcpListener /*  */
+    listener: TcpListener, /*  */
 
-/* config: &cli::Args */
+                           /* config: &cli::Args */
 ) -> eyre::Result<Server, std::io::Error> {
     // debug!("CLI config: {:#?}", config);
     trace!("We are inside the run-function!");
 
-    // needed endpoints
-    // rating/?steam_id=<Steam-ID>
-    // rating/?profile_id=<ageofempires.com-Profile-ID>
-
+    // Create server with Endpoints
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger)
