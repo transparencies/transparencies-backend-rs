b3948ca43f5b99100f7b726684030f85b725b9ad
Simon <simon@systemli.org>
2021-02-22 21:10:31 +0100

    Cleanup

--- src/domain/api_handler/client.rs
+++ src/domain/api_handler/client.rs
@@ -1,36 +1,14 @@
 //! Core client logic of the application
 
-use log::{
-    debug,
-    error,
-    info,
-    trace,
-    warn,
-};
+use log::{debug, error, info, trace, warn};
 use reqwest::Request;
-use stable_eyre::eyre::{
-    eyre,
-    Report,
-    Result,
-    WrapErr,
-};
+use stable_eyre::eyre::{eyre, Report, Result, WrapErr};
 
-use ::serde::{
-    de::DeserializeOwned,
-    Deserialize,
-    Serialize,
-};
-use std::{
-    collections::HashMap,
-    time::Duration,
-};
+use ::serde::{de::DeserializeOwned, Deserialize, Serialize};
+use std::{collections::HashMap, time::Duration};
 
 use crate::domain::api_handler::response::{
-    aoc_ref::{
-        platforms,
-        players,
-        teams,
-    },
+    aoc_ref::{platforms, players, teams},
     aoe2net::last_match::PlayerLastMatch,
 };
 
@@ -208,7 +186,9 @@ impl Default for ApiRequest {
 
 impl ApiRequest {
     pub async fn execute<R>(&self) -> Result<R>
-    where R: for<'de> serde::Deserialize<'de> {
+    where
+        R: for<'de> serde::Deserialize<'de>,
+    {
         Ok(self
             .client
             .get(&format!("{}/{}", &self.root, &self.endpoint))
@@ -219,82 +199,3 @@ impl ApiRequest {
             .await?)
     }
 }
-
-// TODO: Pull file(s) from github
-// e.g. https://raw.githubusercontent.com/SiegeEngineers/aoc-reference-data/master/data/players.yaml
-
-//     /// Fills members of a DataLists object with data deserialised from
-//     /// file paths that were given via the command line arguments
-//     /// players_input_path, teams_input_path, platforms_input_path
-//     pub fn new_from_cli_config(config: cli::Args) -> Result<DataLists,
-// Report> {         // TODO: Error handling
-//         let mut data = DataLists::new();
-
-//         // Deserializing Player file
-//         match config.players_input_path {
-//             Some(k) => {
-//                 debug!("Player file given: {:?}", &k);
-//                 data.player_list =
-//                     DataLists::deserialize_list_to_vec_from_file::<
-//                         players::Players,
-//                     >(&k)
-//                     .expect("Parsing of players file failed.");
-//             }
-//             None => {}
-//         };
-
-//         // Deserializing Teams file
-//         match config.teams_input_path {
-//             Some(k) => {
-//                 debug!("Teams file given: {:?}", &k);
-//                 data.team_list =
-// DataLists::deserialize_list_to_vec_from_file::<teams::Teams>(&k)
-// .expect("Parsing of teams file failed.");             }
-//             None => {}
-//         };
-
-//         // Deserializing Platforms file
-//         match config.platforms_input_path {
-//             Some(k) => {
-//                 debug!("Platforms file given: {:?}", &k);
-//                 data.platform_list =
-//                     DataLists::deserialize_list_to_vec_from_file::<
-//                         platforms::Platforms,
-//                     >(&k)
-//                     .expect("Parsing of platforms file failed.");
-//             }
-//             None => {}
-//         }
-
-//         // Returning DataLists Struct
-//         Ok(data)
-//     }
-
-//     /// Generic function to deserialize a given file with `serde_any`
-//     /// from a path into a vector that contains a special datatype T
-//     fn deserialize_list_to_vec_from_file<T>(
-//         path: &dyn AsRef<Path>
-//     ) -> Result<Vec<T>, Report>
-//     where T: DeserializeOwned {
-//         // Open the file in read-only mode with buffer.
-//         // let file = File::open(&path)?;
-//         // let reader = BufReader::new(file);
-
-//         // Read the list contents of the file as an instance of `Vec<T>`.
-//         let list: Vec<T> = serde_any::from_file(path)
-//             .expect("Parsing of the data file failed.");
-//         Ok(list)
-//     }
-
-// /// Matches the extension of a given filename and
-// /// returns the datatype for serde
-// pub fn get_file_type_from_extension(
-//     ext: &str
-// ) -> Option<serde_any::Format> {
-//     match ext {
-//         "yaml" | "yml" => Some(serde_any::Format::Yaml),
-//         "json" => Some(serde_any::Format::Json),
-//         "toml" => Some(serde_any::Format::Toml),
-//         _ => None,
-//     }
-// }
