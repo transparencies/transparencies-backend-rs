1103a3d04a208df62605cc382d0cd09b8a6e50a9
Simon <simon@systemli.org>
2021-02-28 19:02:25 +0100

    Cleanup

--- src/domain/api_handler/mod.rs
+++ src/domain/api_handler/mod.rs
@@ -2,8 +2,3 @@ pub mod client;
 // TODO: For later implementation of OpenAPI parsing
 // pub mod openapi;
 // pub mod utils;
-
-// TODO:
-// API_HANDLER
-// Get & deserialize yaml into struct
-// https://raw.githubusercontent.com/SiegeEngineers/aoc-reference-data/master/data/players.yaml
