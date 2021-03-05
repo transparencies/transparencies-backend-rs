b017da299b590dc6beae694a1c3ecf9847fd9080
Simon <simon@systemli.org>
2021-03-05 07:18:20 +0100

    More docs

--- src/domain/types/error.rs
+++ src/domain/types/error.rs
@@ -1,6 +1,7 @@
 //! Error types of our library part
 use std::num::ParseIntError;
 
+use displaydoc::Display;
 use thiserror::Error;
 
 use crate::domain::types::{
@@ -8,80 +9,96 @@ use crate::domain::types::{
     GithubFileRequest,
 };
 
-#[derive(Error, Debug)]
+/// Error type for the MatchInfoProcessor
+#[derive(Error, Display, Debug)]
 pub enum ProcessingError {
-    #[error("No candidate for civilisation found.")]
+    /// No candidate for civilisation found.
     CivilisationError,
-    #[error("Assembly of Matchinfo result failed. Result is empty.")]
+    /// Assembly of Matchinfo result failed. Result is empty.
     AssemblyError,
-    #[error("Responder experienced an error.")]
+    /// Responder experienced an error.
     ResponderMalfunction(#[from] ResponderError),
-    #[error("Parsing of Integer data failed")]
+    /// Parsing of Integer data failed
     ProcessIntParsingError(#[from] ParseIntError),
-    #[error("Conversion to String failed.")]
+    /// Conversion to String failed.
     SerdeStringConversionError(#[from] serde_json::Error),
-    #[error("Dividing by Zero is not allowed.")]
+    /// Dividing by Zero is not allowed.
     DividingByZeroError,
-    #[error("Haven't found a rating for player id: {0}")]
+    /// Haven't found a rating for player id: {0}
     LookupRatingNotFound(i64),
-    #[error("Haven't found a leaderboard value for player id: {0}")]
+    /// Haven't found a leaderboard value for player id: {0}
     LeaderboardNotFound(i64),
-    #[error("Haven't found a translation for {0}: {1}")]
+    /// Haven't found a translation for {0}: {1}
     TranslationError(String, usize),
 }
 
-#[derive(Error, Debug)]
+/// Error type for the MatchInfoResponder
+#[derive(Error, Display, Debug)]
 pub enum ResponderError {
-    #[error(
-        "Request {req:?} with {name:?} is not matching any response name."
-    )]
-    RequestNotMatching { name: String, req: ApiRequest },
-    #[error("Data for `{0}` not found.")]
+    /// Request {req:?} with {name:?} is not matching any response name.
+    RequestNotMatching {
+        /// Request name
+        name: String,
+        /// Request itself
+        req: ApiRequest,
+    },
+    /// Data for `{0}` not found.
     NotFound(String),
-    #[error("HTTP-Client experienced an error.")]
+    /// HTTP-Client experienced an error.
     HttpClientError(#[from] reqwest::Error),
-    #[error("Parsing of Integer data failed")]
+    /// Parsing of Integer data failed
     RespondIntParsingError(#[from] ParseIntError),
-    #[error("Haven't found a translation for {0}: {1}")]
+    /// Haven't found a translation for {0}: {1}
     TranslationError(String, usize),
-    #[error("Couldn't get the value of the translation string: {0} at given index {1}")]
+    /// Couldn't get the value of the translation string: {0} at given index
+    /// {1}
     TranslationPosError(String, usize),
-    #[error("Couldn't get the value of the translation string, because it has already been moved.")]
+    /// Couldn't get the value of the translation string, because it has
+    /// already been moved.
     TranslationHasBeenMovedError,
 }
 
-#[derive(Error, Debug)]
+/// Error type for a FileRequest
+#[derive(Error, Display, Debug)]
 pub enum FileRequestError {
-    #[error(
-        "Request {req:?} with {name:?} is not matching any response name."
-    )]
+    /// Request {req:?} with {name:?} is not matching any response name.
     RequestNotMatching {
+        /// Request name
         name: String,
+        /// Request itself
         req: GithubFileRequest,
     },
-    #[error("HTTP-Client experienced an error.")]
+    /// HTTP-Client experienced an error.
     HttpClientError(#[from] reqwest::Error),
 }
 
-#[derive(Error, Debug)]
+/// Error type for an ApiRequest
+#[derive(Error, Display, Debug)]
 pub enum ApiRequestError {
-    #[error(
-        "Request {req:?} with {name:?} is not matching any response name."
-    )]
-    RequestNotMatching { name: String, req: ApiRequest },
-    #[error("HTTP-Client experienced an error.")]
+    /// Request {req:?} with {name:?} is not matching any response name.
+    RequestNotMatching {
+        /// Request name
+        name: String,
+        /// Request itself
+        req: ApiRequest,
+    },
+    /// HTTP-Client experienced an error.
     HttpClientError(#[from] reqwest::Error),
 }
 
-#[derive(Error, Debug)]
+/// Error type for the Indexing functionality
+#[derive(Error, Display, Debug)]
 pub enum IndexingError {
-    #[error(
-        "Player {name:?} with Profile ID {profile_id:?} does already exist in the index at position {pos:?}, doublette is {doublette:?}."
-    )]
+    /// Player {name:?} with Profile ID {profile_id:?} does already exist in
+    /// the index at position {pos:?}, doublette is {doublette:?}.
     PlayerAlreadyExisting {
+        /// Player name
         name: String,
+        /// profile_id of the corresponding Player
         profile_id: String,
+        /// Position in Vector
         pos: usize,
+        /// Position of doublette
         doublette: usize,
     },
 }
