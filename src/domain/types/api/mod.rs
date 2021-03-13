4aa0f979d193dc38fae82f6ebc55d10523019877
simonsan <14062932+simonsan@users.noreply.github.com>
2021-03-13 23:55:38 +0100

    Implement requesting with `match_id` as `id_type` (#19)

--- src/domain/types/api/mod.rs
+++ src/domain/types/api/mod.rs
@@ -20,6 +20,7 @@ use std::{
     },
     path::PathBuf,
 };
+
 /// Datastructure for an incoming `request` on our api
 /// on the `matchinfo` endpoint
 #[derive(Debug, Default, Serialize, Deserialize, Clone)]
@@ -28,7 +29,8 @@ pub struct MatchInfoRequest {
     pub language: Option<String>,
     /// Requested game (Optional), Standard value is "aoe2de"
     pub game: Option<String>,
-    /// Requested type of ID, possible values are ["steam_id", "profile_id"]
+    /// Requested type of ID, possible values are ["steam_id", "profile_id",
+    /// "match_id"]
     pub id_type: String,
     /// The ID itself as a String
     pub id_number: String,
