a88be1b5a3dab9ee14eef5c490f4136376229f66
Simon <simon@systemli.org>
2021-03-17 01:58:48 +0100

    Remove github api folder as we use the old approach for the files

--- crates/api/github/src/lib.rs
+++
@@ -1,4 +0,0 @@
-pub mod endpoints;
-pub mod types;
-
-pub const AOE2_NET_URL: &'static str = "https://raw.githubusercontent.com";
--- crates/api/github/src/types/error.rs
+++
@@ -1,9 +0,0 @@
-use displaydoc::Display;
-use thiserror::Error;
-
-/// Error thrown from aoe2.net API endpoints
-#[derive(Debug, Error, Display)]
-pub enum ApiError {
-    /// Generic Error thrown by the API
-    Generic(String),
-}
